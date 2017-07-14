06be2b3d65406d9e8c8b09b8b81522f5b46f5ee4
kumelkalen <free.your.mind.fc@gmail.com>
2017-07-14 12:18:36 -0400

    Hacemos las validaciones

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 /* Holiii acá va tu código también */
+$(document).ready(function(){
+	
+	$(".errorTxt2").hide();
+	$(".errorTxt").hide();
+	$("#iniciar-sesion").click(function(e) {
+		var user = $("#email").val();
+		console.log(user);
+		var contraseña = $("#password").val();
+		console.log(contraseña);
+		//Validación email
+		function validateEmail(email) {
+			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(email);
+		}
+		if(user.length = 0 || validateEmail(user) == false){
+			$(".errorTxt").show();
+		}
+		if(contraseña.length = 0 || contraseña.length < 5){
+			$(".errorTxt2").show();
+		}else{
+			e.preventDefault();
+			location.href = "opciones.html";
+		}
+	});
+});
